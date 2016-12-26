c06819de64221175faf2e0b5f9966ccd83fa1cd3
misCC <1518265554@qq.com>
2016-12-26 09:53:23 +0800

    update

--- app/src/main/java/com/example/a/myapplication/MainActivity.java
+++ app/src/main/java/com/example/a/myapplication/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.a.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button loginButton;
+    private Button btn1;
+    private Button btn2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,17 +18,35 @@ public class MainActivity extends AppCompatActivity {
         // 将布局xml文件引入到 activity 中
         setContentView(R.layout.activity_main);
 
+        btn1 = (Button) findViewById(R.id.button1);
+        btn2 = (Button) findViewById(R.id.button2);
+
         /**
-         * init 当前所需要的控件
-         * @return 返回的是一个 view 的对象
+         * 点击事件外部类写法和作用
          */
-        loginButton =(Button)findViewById(R.id.button1);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+        btn1.setOnClickListener(new MyOnClickListener() {
             @Override
             public void onClick(View v) {
-                // 在当前onClick方法中监听点击Button的动作
-                System.out.println("我的按钮被点击了");
+                // 调用父类的onclick事件
+                super.onClick(v);
+                Toast.makeText(MainActivity.this, "btn1要执行的逻辑", 1).show();
             }
         });
+        btn2.setOnClickListener(new MyOnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 调用父类的onclick事件
+                super.onClick(v);
+                Toast.makeText(MainActivity.this, "btn2要执行的逻辑", 1).show();
+            }
+        });
+    }
+}
+
+class MyOnClickListener implements View.OnClickListener {
+    @Override
+    public void onClick(View v) {
+        v.setAlpha(0.5f);
+        Log.i("tag", "父类的onClick事件");
     }
 }
