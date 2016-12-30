e6057c1be5075a74d7462d31b7d9393f046d8106
misCC <1518265554@qq.com>
2016-12-30 14:23:15 +0800

    test list view

---
+++ app/src/main/java/com/example/a/myapplication/ListView.java
@@ -0,0 +1,12 @@
+package com.example.a.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ListView extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.list_view);
+    }
+}
